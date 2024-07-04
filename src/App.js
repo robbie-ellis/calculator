6966a4080b576169d5de5c5a9981c77abbb13d86
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-07-04 16:36:55 -0700

    Completed Calculator. Passes all tests.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -6,11 +6,24 @@ function App() {
   const [input, setInput] = useState("0");
   const [operators, setOperators] = useState([]);
   const [operands, setOperands] = useState([]);
+  const [shouldCalculate, setShouldCalculate] = useState(false);
+
+  const operandsRef = useRef(operands);
+  const operatorsRef = useRef(operators);
+
+  useEffect(() => {
+    operandsRef.current = operands;
+  }, [operands]);
+
+  useEffect(() => {
+    operatorsRef.current = operators;
+  }, [operators]);
 
   function handleNumberInput(event) {
     const value = event.target.value;
-    console.log(value);
     if (input === "0" && value === "0") {
+      return;
+    } else if (input === "-" && value === "0") {
       return;
     } else if (input === "0") {
       setInput(() => value);
@@ -19,33 +32,87 @@ function App() {
     }
   }
 
-  function clear() {
+  function clearAll() {
     setInput(() => "0");
     setOperators(() => []);
     setOperands(() => []);
   }
 
-  /*
-  function handleOperatorInput(event) {
-    const value = event.target.value;
-    switch (value) {
-      case "/":
-        setOperands((prevOperands) => [...prevOperands, Number(input)]);
-        setInput(() => "0");
-        setOperators(() => [value]);
-        break;
-      case "-":
-        if (operands.length < 1 && input === "0") {
+  function clearOps() {
+    setOperators(() => []);
+    setOperands(() => []);
+  }
 
-        }  
+  useEffect(() => {
+    if (shouldCalculate) {  
+      console.log("Calc begins");
+      console.log("Operators: " + operatorsRef.current);
+      console.log("Operands: " + operandsRef.current);
+      let result = Number(operandsRef.current[0]);
+      if (operatorsRef.current.length === operandsRef.current.length) {
+        console.log("Slicing");
+        setOperators((prevOperators) => [...prevOperators].slice(0, -1));
+      }
+      for (let i = 0; i < operatorsRef.current.length; i++) {
+        switch(operatorsRef.current[i]) {
+          case "/":
+            result /= Number(operandsRef.current[i + 1]);
+            break;
+          case "*":
+            result *= Number(operandsRef.current[i + 1]);
+            break;
+          case "+":
+            result += Number(operandsRef.current[i + 1]);
+            break;
+          case "-":
+            result -= Number(operandsRef.current[i + 1]);
+            break;
+          default:
+            //This should never be reached
+        }
+      }
+      setInput(() => result);
+      setShouldCalculate(() => false);
+      clearOps();
+    }
+  }, [shouldCalculate]);
+
+  function handleOperators(event) {
+    const value = event.target.value;
+    //For handling negative numbers
+    if (input === "" && value === "-") {
+      setInput(() => value);
+    } 
+    if (input === "-") {
+      setOperators((prevOperators) => [...prevOperators.slice(0, -1), value]);
+      setInput(() => "");
+    } else if (!(input === "")) {
+      setOperands((prevOperands) => [...prevOperands, input]);
+      setOperators((prevOperators) => [...prevOperators, value]);
+      setInput(() => "");
+    } else if (input === "" && operators.length > 0) {
+      if (value === "/" || value === "*" || value === "+")
+      setOperators((prevOperators) => [...prevOperators.slice(0, -1), value]);
     }
   }
-  */
-
+  
   function handleEquals() {
-
+    if (input === "" || input === "-") {
+      return;
+    } else if (operands.length < 1) {
+      setOperands((prevOperands) => [...prevOperands, input])
+      return;
+    } else {
+      setOperands((prevOperands) => [...prevOperands, input])
+      setShouldCalculate(() => true);
+    }
   }
 
+  useEffect(() => {
+    console.log(operands);
+    console.log(operators);
+  }, [operands, operators]);
+  
   function handleDecimalInput() {
     if (input.includes(".")) {
       return;
@@ -65,34 +132,34 @@ function App() {
           </div>
         </div>
         <div className='row row-padding'>
-          <div className='col p-0'><button id="clear" onClick={clear} className='btn btn-dark w-100'>Clear</button></div>
+          <div className='col p-0'><button id="clear" onClick={clearAll} className='btn btn-dark w-100'>Clear</button></div>
         </div>
         <div className='row row-padding'>
           <div className='col-sm-9 p-0'></div>
-          <div className='col-sm-3 p-0'><button id="divide" value="/" className='operator-button btn w-100'>/</button></div>
+          <div className='col-sm-3 p-0'><button id="divide" value="/" onClick={handleOperators} className='operator-button btn w-100'>/</button></div>
         </div>
         <div className='row row-padding'>
           <div className='col-sm-3 p-0'><button id="seven" value="7" onClick={handleNumberInput} className='numeric-button btn w-100'>7</button></div>
           <div className='col-sm-3 p-0'><button id="eight" value="8" onClick={handleNumberInput} className='numeric-button btn w-100'>8</button></div>
           <div className='col-sm-3 p-0'><button id="nine" value="9" onClick={handleNumberInput} className='numeric-button btn w-100'>9</button></div>
-          <div className='col-sm-3 p-0'><button id="multiply" value="*" className='operator-button btn w-100'>*</button></div>
+          <div className='col-sm-3 p-0'><button id="multiply" value="*" onClick={handleOperators} className='operator-button btn w-100'>*</button></div>
         </div>
         <div className='row row-padding'>
           <div className='col-sm-3 p-0'><button id="four" value="4" onClick={handleNumberInput} className='numeric-button btn w-100'>4</button></div>
           <div className='col-sm-3 p-0'><button id="five" value="5" onClick={handleNumberInput} className='numeric-button btn w-100'>5</button></div>
           <div className='col-sm-3 p-0'><button id="six" value="6" onClick={handleNumberInput} className='numeric-button btn w-100'>6</button></div>
-          <div className='col-sm-3 p-0'><button id="subtract" value="-" className='operator-button btn w-100'>-</button></div>
+          <div className='col-sm-3 p-0'><button id="subtract" value="-" onClick={handleOperators} className='operator-button btn w-100'>-</button></div>
         </div>
         <div className='row row-padding'>
           <div className='col-sm-3 p-0'><button id="one" value="1" onClick={handleNumberInput} className='numeric-button btn w-100'>1</button></div>
           <div className='col-sm-3 p-0'><button id="two" value="2" onClick={handleNumberInput} className='numeric-button btn w-100'>2</button></div>
           <div className='col-sm-3 p-0'><button id="three" value="3" onClick={handleNumberInput} className='numeric-button btn w-100'>3</button></div>
-          <div className='col-sm-3 p-0'><button id="add" value="+" className='operator-button btn w-100'>+</button></div>  
+          <div className='col-sm-3 p-0'><button id="add" value="+" onClick={handleOperators} className='operator-button btn w-100'>+</button></div>  
         </div>
         <div className='row row-padding'>
           <div className='col-sm-3 p-0'><button id="decimal" onClick={handleDecimalInput} className='operator-button btn w-100'>.</button></div>
           <div className='col-sm-3 p-0'><button id="zero" value="0" onClick={handleNumberInput} className='numeric-button btn w-100'>0</button></div>
-          <div className='col-sm-6 p-0'><button id="equals" className='btn btn-dark w-100'>=</button></div>          
+          <div className='col-sm-6 p-0'><button id="equals" onClick={handleEquals} className='btn btn-dark w-100'>=</button></div>          
         </div>
       </div>
     </div>
